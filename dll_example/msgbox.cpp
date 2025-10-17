11fd92dd7e499e35e385b3895db8b7a02ca5b3ca
gdziewon <e.dziewonski@gmail.com>
2025-10-17 13:39:25 -0700

    Add dll example to repo

---
+++ dll_example/msgbox.cpp
@@ -0,0 +1,27 @@
+#include <Windows.h>
+
+DWORD WINAPI thread(LPVOID lpParam) {
+    HMODULE hModule = (HMODULE)lpParam;
+    MessageBoxW(nullptr, L"meow1337", L"injected", MB_ICONINFORMATION);
+    FreeLibraryAndExitThread(hModule, 0);
+
+    return 0;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(nullptr, 0, thread, hModule, 0, nullptr);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
